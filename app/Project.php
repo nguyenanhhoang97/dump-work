cb8b3453f0054577c70225dc9b6ba3b644f2ef8d
Anh Hoang Nguyen <nguyenanhhoang.ict@gmail.com>
2019-09-22 16:31:20 +0700

    alter projects tb, create project model

--- app/Project.php
+++ app/Project.php
@@ -3,8 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
 class Project extends Model
 {
-    //
+    protected $table = 'projects';
+
+    public $timestamps = true;
+
+    public static function getAllProjects($pageIndex, $pageSize, $search)
+    {
+        $offset = $pageIndex * $pageSize;
+        return DB::table('projects')
+            ->where('project_name', 'like', '%' . $search . '%')
+            ->offset($offset)
+            ->limit($pageSize)
+            ->get();
+    }
 }
