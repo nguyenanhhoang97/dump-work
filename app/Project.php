c6fd634014b1eeb7a9a05c219798ca1c21243fb1
tuangit1207 <galacticlevi07@gmail.com>
2019-09-23 23:39:39 +0700

    function restore and edit query getAll

--- app/Project.php
+++ app/Project.php
@@ -11,14 +11,15 @@ class Project extends Model
 
     public $timestamps = true;
 
-    public static function getAllProjects($pageIndex, $pageSize, $search)
+    public static function getAllProjects($pageIndex, $pageSize, $search, $is_deleted)
     {
+
         $offset = $pageIndex * $pageSize;
         return DB::table('projects')
             ->where('project_name', 'like', '%' . $search . '%')
             ->offset($offset)
             ->limit($pageSize)
-            ->get();
+            ->get($offset !== $is_deleted);
     }
     public static function getProjectById($id)
     {
@@ -71,7 +72,21 @@ class Project extends Model
         $guarantee
     ) {
         return DB::table('projects')
-            ->insert(['project_name' => $project_name, 'team_size' => $team_size, 'git_url' => $git_url, 'excution_time' => $excution_time, 'cost' => $cost, 'incom' => $incom, 'guarantee' => $guarantee
+            ->insert([
+                'project_name' => $project_name, 'team_size' => $team_size, 'git_url' => $git_url, 'excution_time' => $excution_time, 'cost' => $cost, 'incom' => $incom, 'guarantee' => $guarantee
             ]);
+    }
+
+    public static function restoreProjects($id, $offset)
+    {
+        return DB::table('projects')
+            ->where('id', $id)
+            ->update([
+                'is_deleted' => true
+            ]);
+            
+        if ('is_deleted' != true) {
+            return $offset;
+        }
     }
 }
