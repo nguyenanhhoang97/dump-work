796b0606b003392bea3cdaad7a77b2590bd78446
Anh Hoang Nguyen <nguyenanhhoang.ict@gmail.com>
2019-09-24 00:41:36 +0700

    WIP: layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,13 @@
 import Vue from "vue";
-import router from './router'
+import router from "./router";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 import App from "./components/App";
 
+Vue.use(ElementUI);
+
 new Vue({
-    el: "#app",
-    components: { App },
-    router
+  el: "#app",
+  components: { App },
+  router
 });
